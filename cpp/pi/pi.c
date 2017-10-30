c03449a75ee895ab4bc1e19d2f254487ca824ae5
Todd Gaunt <toddgaunt@protonmail.ch>
2017-10-30 14:28:36 -0400

    Revert changes to pi

--- cpp/pi/pi.c
+++ cpp/pi/pi.c
@@ -1,133 +1,135 @@
-/* See LICENSE file for copyright and license details */
-/**
- * Authors:
- *	Philip J. Hatcher
- *	Todd O. Gaunt
- * Description:
- *	Concurrently compute an approximation of pi.
- */
+// pi4.c (Uses Posix threads on Linux, Sept 12)
+// child threads put partial sum in an array
+// thread_join used to have main thread wait
+// main thread sums values in array to get final sum
 
-#include <errno.h>
-#include <pthread.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
-#define PROG_NAME "pi"
-
-#define EPRINT(...) \
-	do { \
-		fprintf(stderr, PROG_NAME": error: "__VA_ARGS__); \
-		exit(EXIT_FAILURE); \
-	} while (0);
-
-/* How many intervals to divide [0,1] into */
+// how many intervals to divide [0,1] into
 #define INTERVALS 50000000
-/* Width of an interval */
-#define WIDTH (1.0 / INTERVALS)
-/* The maximum number of child threads that can be used */
-#define THREAD_MAX 24
 
-/* Number of intervals per thread */
-size_t chunk;
-/* Processor IDs less than split have one extra interval */
-size_t split;
-/* Child threads drop values into the array */
-double partial_sum[THREAD_MAX];
+// width of an interval
+#define WIDTH (1.0/INTERVALS)
 
-/* Fits inside a register */
-struct worker_context {
-	/* Beginning index to use for the partial_sum array */
-	uint32_t index;
-	/* Number of sums to calculate */
-	uint32_t n_sums;
-};
+// the maximum number of child threads that can be used
+#define MAX_T 24
 
-typedef struct worker_context worker_context;
+// array for storing thread IDs
+pthread_t pt[MAX_T];
 
-void
-usage()
+// child threads drop values into the array
+double partialSums[MAX_T];
+
+// number of intervals per thread
+int chunk;
+
+// processor IDs less than split have one extra interval
+int split;
+
+// prints usage message if command line arguments are not what is expected
+// program expects a single argument: the number of child threads to create
+void usage(char *);
+
+// prints an error message and terminates the program
+void error(char *);
+
+// this is the work executed by each child thread
+void * work(void *);
+
+int main (int argc, char *argv[])
 {
-	fprintf(stderr, "Usage: "PROG_NAME" [NUMBER OF THREADS]\n");
-	exit(EXIT_FAILURE);
+   long i;
+   int n; // number of child threads to create */
+
+   if (argc !=2) usage("pi numofthreads\n");
+
+   n = atoi(argv[1]);
+
+   if (n <= 0) usage("number of threads must be > 0");
+
+   if (n > MAX_T) usage("too many threads requested");
+
+   printf("%d (child) threads used\n", n);
+
+   chunk = INTERVALS / n;
+   split = INTERVALS % n;
+   if (split == 0)
+   {
+      split = n;
+      chunk -= 1;
+   }
+
+   // create n threads
+   for (i=0; i < n; i++)
+   {
+      /* create threads; DANGER: thread logical id (int) passed as "void *" */
+      if (pthread_create(&pt[i], NULL, work, (void *) i) != 0)
+         error("error in thread create");
+   }
+
+   double sum = 0.0;
+
+   // wait for each thread to finish
+   for (i=0; i < n; i++)
+   {
+     if (pthread_join(pt[i], NULL))
+     {
+       error("error in thread join");
+     }
+     sum += partialSums[i];
+   }
+   sum *= 1.0/INTERVALS;
+
+   printf ("Estimation of pi is %14.12f\n", sum);
+
+   return 0;
 }
 
-void *
-work(void *in)
+void * work(void * in)
 {
-	size_t i;
-	size_t j;
-	/* First interval to be processed */
-	size_t low;                 
-	/* First interval *not* to be processed */
-	size_t high;              
-	/* Sum for intervals being processed */
-	double localSum = 0.0;   
-	/* Mid-point of an interval */
-	double x;                
-	/* Retrieve the thread context passed in */
-	worker_context wc = (*((worker_context *) &in));
+   int i;
+   int low;                 // first interval to be processed
+   int high;                // first interval *not* to be processed
+   double localSum = 0.0;   // sum for intervals being processed
+   double x;                // mid-point of an interval
+   long id = (long) in;     // logical thread id (0..n-1)
 
-	/* Calculate n_sums partial sums and store them into the array starting
-	 * at the index provided by worker_context */
-	for (i = wc.index; i < wc.index + wc.n_sums; ++i) {
-		if (i < split) {
-			low = (i * (chunk + 1));
-			high = low + (chunk + 1);
-		} else {
-			low = (split * (chunk + 1)) + ((i - split) * chunk);
-			high = low + chunk;
-		}
+   if (id < split)
+   {
+      low = (id * (chunk + 1));
+      high = low + (chunk + 1);
+   }
+   else
+   {
+      low = (split * (chunk + 1)) + ((id - split) * chunk);
+      high = low + chunk;
+   }
 
-		localSum = 0.0;
-		x = (low+0.5)*WIDTH;
-		for (j = low; j < high; ++j) {
-			localSum += (4.0/(1.0+x*x));
-			x += WIDTH;
-		}
-		partial_sum[i] = localSum;
-	}
-	return 0;
+   x = (low+0.5)*WIDTH;
+   for (i = low; i < high; i++)
+   {
+      localSum += (4.0/(1.0+x*x));
+      x += WIDTH;
+   }
+
+   partialSums[id] = localSum;
+
+   return 0;
 }
 
-int
-main(int argc, char **argv)
+void error(char *str)
 {
-	size_t i = 0;
-	/* Number of threads, default number is 2 */
-	size_t n_threads = 4;
-	/* Number of partial sums to calculate, default is 2 */
-	size_t n = 16;
-	double sum = 0.0;
-	pthread_t pid[THREAD_MAX] = {0};
-	uint32_t n_sums = n / n_threads;
-
-	//TODO(todd): Start timer here.
-	chunk = INTERVALS / n;
-	split = INTERVALS % n;
-	if (0 == split) {
-		split = n;
-		chunk -= 1;
-	}
-	/* Create the worker threads */
-	for (i = 0; i < n_threads; ++i) {
-		worker_context wc = {i * n_sums, n_sums};
-		/* Cast magic */
-		long k = *((size_t *) &wc);
-		if (0 != pthread_create(&pid[i], NULL, work, (void *) k))
-			EPRINT("could not create thread\n");
-	}
-	// wait for each thread to finish
-	for (i = 0; i < n_threads; i++) {
-		if (pthread_join(pid[i], NULL))
-			EPRINT("could not join thread\n");
-		for (size_t j = i * n_sums; j < i * n_sums + n_sums; ++j) {
-			sum += partial_sum[j];
-		}
-	}
-	sum *= 1.0 / INTERVALS;
-	//TODO(todd): Stop timer here.
-	printf("Estimation of pi is %14.12f\n", sum);
-	return 0;
+    perror(str);
+    exit(-1);
 }
+
+void usage(char *str)
+{
+   fprintf(stderr, "usage: %s\n",str);
+   exit(-1);
+}
+
