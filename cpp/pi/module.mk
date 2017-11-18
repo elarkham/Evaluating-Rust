c72ac7e73f4c2d10a3e81ce41004f386dc673db0
Todd Gaunt <toddgaunt@protonmail.ch>
2017-11-18 00:40:21 -0500

    Modify C++ pi build to produce both an -Os and -O2 binary

--- cpp/pi/module.mk
+++ cpp/pi/module.mk
@@ -1,6 +1,9 @@
 SRC += pi.cpp
-TARGET += $(BUILD)/pi/pi
+TARGET += $(BUILD)/pi/pi_Os $(BUILD)/pi/pi_O2
 LDFLAGS += -lpthread
 
-$(BUILD)/pi/pi: $(BUILD)/pi/pi.o
+$(BUILD)/pi/pi_Os: $(BUILD)/pi/pi_Os.o
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
+
+$(BUILD)/pi/pi_O2: $(BUILD)/pi/pi_O2.o
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
