f44df5220675267f486ec9f5502ae2c403254354
Todd Gaunt <toddgaunt@protonmail.ch>
2017-12-02 17:30:09 -0500

    Add mkgraph utility (for graphing benchmark data)

---
+++ bin/mkgraph.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import matplotlib.pyplot as plt
+
+def mklist(filename):
+    acc = []
+    with open(filename, "r") as f:
+        for line in f:
+            acc.append(float(line))
+    return acc
+
+if (len(sys.argv) != 3):
+    print("usage: mkgraph soa aos")
+    exit(-1)
+
+soa = mklist(sys.argv[1])
+aos = mklist(sys.argv[2])
+x_range = range(1, min(len(soa), len(aos)) * 1000, 1000)
+
+plt.plot(x_range, soa, 'g--', x_range, aos, 'r--')
+plt.ylabel("duration in seconds")
+plt.xlabel("size of dataset")
+plt.show()
