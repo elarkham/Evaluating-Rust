9bc63323ba596197693128af0342ee90e405efff
Todd Gaunt <toddgaunt@protonmail.ch>
2017-12-02 17:29:49 -0500

    Fix bounds on rust jacobi

--- rust/jacobi/src/main.rs
+++ rust/jacobi/src/main.rs
@@ -10,33 +10,33 @@
  **	Using Pthreads.
  */
 
-use std::thread;
 //extern crate time;
+
+use std::thread;
 //use time::PreciseTime;
-extern crate crossbeam;
 
 const SIZE : usize = 1280;
 const TEMP : f64 = 50.0;
 const EPSILON : f64 = 0.1;
 
 fn main () {
-
+    let n_threads = 4;
     let mut children = vec![];
-    let split = SIZE / 4;
-    for i in 0..4 {
+    let split = SIZE / n_threads;
+    for i in 0..n_threads {
         children.push(thread::spawn(move || {
-            let mut new : Vec<Vec<f64>> = vec![vec![0.0; split]; split];
-            let mut old : Vec<Vec<f64>> = vec![vec![0.0; split + 2]; split];
+            let mut new : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split];
+            let mut old : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split + 1];
 
             /* Initialize the temperatures */
-            for i in  1..(split - 1) {
+            for i in  1..(SIZE - 1) {
                 /* South Boundry */
                 old[split-1][i] = 100.0;
                 new[split-1][i] = 100.0;
             }
             for i in 1..(split - 1) {
                 /* Inner core */
-                for j in 1..(split - 1) {
+                for j in 1..(SIZE - 1) {
                     old[i][j] = 50.0;
                     new[i][j] = 50.0;
                 }
@@ -44,7 +44,7 @@ fn main () {
             loop {
                 let mut maxerr = 0.0;
                 for i in 1..split - 1 {
-                    for j in 1..split - 1{
+                    for j in 1..SIZE - 1{
                         new[i][j] = (old[i-1][j] + old[i+1][j] +
                                      old[i][j+1] + old[i][j-1]) / 4.0;
                         let change = (old[i][j]-new[i][j]).abs();
@@ -54,7 +54,7 @@ fn main () {
                     }
                 }
                 for i in 1..split - 1 {
-                    for j in 1..split - 1 {
+                    for j in 1..SIZE - 1 {
                         old[i][j] = new[i][j];
                     }
                 }
@@ -62,15 +62,22 @@ fn main () {
                     break;
                 }
             }
+            return new;
         }));
     }
+    let mut cool_cells = 0;
     for child in children {
-        child.join();
+        let new = child.join().unwrap();
+        for i in 0..split {
+            for j in 0..SIZE {
+                if (new[i][j] < TEMP) {
+                    cool_cells += 1;
+                }
+            }
+        }
         println!("JOIN!");
     }
-    /*
-       println!("There are {} cells cooler than {} degrees",
-       cool_cells,
-       TEMP);
-       */
+    println!("There are {} cells cooler than {} degrees",
+             cool_cells,
+             TEMP);
 }
