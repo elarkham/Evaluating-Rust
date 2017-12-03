8fdb89d67e46a2f6868a491c2d675c0aed716701
Todd Gaunt <toddgaunt@protonmail.ch>
2017-12-03 16:04:39 -0500

    Add vectorized channel production to jacobi

--- rust/jacobi/src/main.rs
+++ rust/jacobi/src/main.rs
@@ -13,6 +13,8 @@
 //extern crate time;
 
 use std::thread;
+use std::sync::mpsc;
+use std::sync::mpsc::{Sender, Receiver};
 //use time::PreciseTime;
 
 const SIZE : usize = 1280;
@@ -23,39 +25,68 @@ fn main () {
     let n_threads = 4;
     let mut children = vec![];
     let split = SIZE / n_threads;
+    let mut channels : Vec<
+        (Sender<(i32, Vec<f64>)>,
+        Receiver<(i32, Vec<f64>)>)> = vec![];
     for i in 0..n_threads {
+        channels.push(mpsc::channel());
+    }
+    for i in 0..n_threads {
+        let tx = channels[i + 1].0.clone();
         children.push(thread::spawn(move || {
             let mut new : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split];
-            let mut old : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split + 1];
+            let mut old : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split + 2];
 
-            /* Initialize the temperatures */
-            for i in  1..(SIZE - 1) {
-                /* South Boundry */
-                old[split-1][i] = 100.0;
-                new[split-1][i] = 100.0;
+            /* The first thread started contains the northern boundry */
+            if 0 == i {
+                for i in 0..split {
+                    new[0][i] = 0.0;
+                }
+                for i in 1..split {
+                    /* Inner core */
+                    for j in 1..(SIZE - 1) {
+                        new[i][j] = 50.0;
+                    }
+                }
             }
-            for i in 1..(split - 1) {
-                /* Inner core */
-                for j in 1..(SIZE - 1) {
-                    old[i][j] = 50.0;
-                    new[i][j] = 50.0;
+            /* Only the last thread started contains the south boundry */
+            if n_threads == i {
+                /* Initialize the temperatures */
+                for i in  1..(SIZE - 1) {
+                    /* South Boundry */
+                    new[split-1][i] = 100.0;
+                }
+                for i in 0..(split - 1) {
+                    /* Inner core */
+                    for j in 1..(SIZE - 1) {
+                        new[i][j] = 50.0;
+                    }
+                }
+            } else {
+                for i in 0..split {
+                    /* Inner core */
+                    for j in 1..(SIZE - 1) {
+                        new[i][j] = 50.0;
+                    }
                 }
             }
             loop {
+                for i in 1..split - 1 {
+                    for j in 1..SIZE - 1 {
+                        old[i + 1][j] = new[i][j];
+                    }
+                }
                 let mut maxerr = 0.0;
                 for i in 1..split - 1 {
                     for j in 1..SIZE - 1{
-                        new[i][j] = (old[i-1][j] + old[i+1][j] +
-                                     old[i][j+1] + old[i][j-1]) / 4.0;
-                        let change = (old[i][j]-new[i][j]).abs();
+                        new[i][j] = (old[i][j]
+                                     + old[i + 2][j]
+                                     + old[i + 1][j + 1]
+                                     + old[i + 1][j - 1]) / 4.0;
+                        let change = (old[i + 1][j] - new[i][j]).abs();
                         if maxerr < change {
                             maxerr =  change; 
                         }
-                    }
-                }
-                for i in 1..split - 1 {
-                    for j in 1..SIZE - 1 {
-                        old[i][j] = new[i][j];
                     }
                 }
                 if EPSILON >= maxerr {
@@ -67,17 +98,17 @@ fn main () {
     }
     let mut cool_cells = 0;
     for child in children {
-        let new = child.join().unwrap();
+        let chunk = child.join().unwrap();
         for i in 0..split {
             for j in 0..SIZE {
-                if (new[i][j] < TEMP) {
+                if chunk[i][j] < TEMP {
                     cool_cells += 1;
                 }
             }
         }
-        println!("JOIN!");
+        eprintln!("JOIN!");
     }
-    println!("There are {} cells cooler than {} degrees",
+    eprintln!("There are {} cells cooler than {} degrees",
              cool_cells,
              TEMP);
 }
