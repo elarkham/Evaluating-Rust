8436c35f100d6e1b311cad3412ac0072fd7db018
Todd Gaunt <toddgaunt@protonmail.ch>
2017-12-04 08:34:36 -0500

    Native Rust concurrent jacobi implementation done

--- rust/jacobi/src/main.rs
+++ rust/jacobi/src/main.rs
@@ -13,8 +13,6 @@
 //extern crate time;
 
 use std::thread;
-use std::sync::mpsc;
-use std::sync::mpsc::{Sender, Receiver};
 //use time::PreciseTime;
 
 const SIZE : usize = 1280;
@@ -23,92 +21,102 @@ const EPSILON : f64 = 0.1;
 
 fn main () {
     let n_threads = 4;
-    let mut children = vec![];
     let split = SIZE / n_threads;
-    let mut channels : Vec<
-        (Sender<(i32, Vec<f64>)>,
-        Receiver<(i32, Vec<f64>)>)> = vec![];
-    for i in 0..n_threads {
-        channels.push(mpsc::channel());
-    }
-    for i in 0..n_threads {
-        let tx = channels[i + 1].0.clone();
-        children.push(thread::spawn(move || {
-            let mut new : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split];
-            let mut old : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; split + 2];
+    let mut new : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; SIZE];
+    let mut old : Vec<Vec<f64>> = vec![vec![0.0; SIZE]; SIZE];
 
-            /* The first thread started contains the northern boundry */
-            if 0 == i {
-                for i in 0..split {
-                    new[0][i] = 0.0;
-                }
-                for i in 1..split {
-                    /* Inner core */
-                    for j in 1..(SIZE - 1) {
-                        new[i][j] = 50.0;
-                    }
-                }
-            }
-            /* Only the last thread started contains the south boundry */
-            if n_threads == i {
-                /* Initialize the temperatures */
-                for i in  1..(SIZE - 1) {
-                    /* South Boundry */
-                    new[split-1][i] = 100.0;
-                }
-                for i in 0..(split - 1) {
-                    /* Inner core */
-                    for j in 1..(SIZE - 1) {
-                        new[i][j] = 50.0;
-                    }
-                }
-            } else {
-                for i in 0..split {
-                    /* Inner core */
-                    for j in 1..(SIZE - 1) {
-                        new[i][j] = 50.0;
-                    }
-                }
-            }
-            loop {
-                for i in 1..split - 1 {
-                    for j in 1..SIZE - 1 {
-                        old[i + 1][j] = new[i][j];
-                    }
-                }
-                let mut maxerr = 0.0;
-                for i in 1..split - 1 {
-                    for j in 1..SIZE - 1{
-                        new[i][j] = (old[i][j]
-                                     + old[i + 2][j]
-                                     + old[i + 1][j + 1]
-                                     + old[i + 1][j - 1]) / 4.0;
-                        let change = (old[i + 1][j] - new[i][j]).abs();
-                        if maxerr < change {
-                            maxerr =  change; 
-                        }
-                    }
-                }
-                if EPSILON >= maxerr {
-                    break;
-                }
-            }
-            return new;
-        }));
+    /* Initialize the temperatures */
+    for i in 1..SIZE {
+        /* Inner core */
+        for j in 1..SIZE - 1 {
+            old[i][j] = 50.0;
+            new[i][j] = 50.0;
+        }
     }
-    let mut cool_cells = 0;
-    for child in children {
-        let chunk = child.join().unwrap();
-        for i in 0..split {
+    for i in  1..SIZE - 1 {
+        /* South Boundry */
+        old[SIZE - 1][i] = 100.0;
+        new[SIZE - 1][i] = 100.0;
+    }
+
+    loop {
+        let mut children = vec![];
+        for id in 0..n_threads {
+            let old = old.clone();
+            let mut new = new.clone();
+            children.push(thread::spawn(move || {
+                let mut beg = id * split;
+                let mut end = beg + split;
+                if id == 0 {
+                    beg += 1;
+                } else if id == n_threads - 1 {
+                    end -= 1;
+                }
+                for i in beg..end {
+                    for j in 1..SIZE - 1 {
+                        new[i][j] = (old[i - 1][j]
+                                     + old[i + 1][j]
+                                     + old[i][j + 1]
+                                     + old[i][j - 1]) / 4.0;
+                    }
+                }
+                return new.clone();
+            }));
+        }
+        let mut maxerr = 0.0;
+        let mut i = 0;
+        for child in children {
+            let rows = child.join().unwrap();
+            let mut beg = i * split;
+            let mut end = beg + split;
+            if i == 0 {
+                beg += 1;
+            } else if i == n_threads - 1 {
+                end -= 1;
+            }
+            for j in beg..end {
+                for k in 1..SIZE - 1 {
+                    new[j][k] = rows[j][k];
+                }
+            }
+            i += 1;
+        }
+        /* Print-out for debugging */
+        /*
+        for i in 0..SIZE {
             for j in 0..SIZE {
-                if chunk[i][j] < TEMP {
-                    cool_cells += 1;
+                print!("{} ", new[i][j]);
+            }
+            println!();
+        }
+        */
+        for i in 1..SIZE - 1 {
+            for j in 1..SIZE - 1 {
+                let change = (old[i][j] - new[i][j]).abs();
+                if maxerr < change {
+                    maxerr =  change; 
                 }
             }
         }
-        eprintln!("JOIN!");
+        if EPSILON >= maxerr {
+            break;
+        }
+        for i in 1..SIZE - 1 {
+            for j in 1..SIZE - 1 {
+                old[i][j] = new[i][j];
+            }
+        }
+    }
+
+    let mut cool_cells = 0;
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            if new[i][j] < TEMP {
+                cool_cells += 1
+            }
+        }
     }
     eprintln!("There are {} cells cooler than {} degrees",
-             cool_cells,
-             TEMP);
+              cool_cells,
+              TEMP);
 }
