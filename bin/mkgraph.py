ff1fb93dc480e36e147627e31cb3242f35c8418b
Ethan Larkham <ethanlarkham@gmail.com>
2017-12-05 18:56:33 -0500

    Tweaked how mkgraph displays data

--- bin/mkgraph.py
+++ bin/mkgraph.py
@@ -16,9 +16,11 @@ if (len(sys.argv) != 3):
 
 soa = mklist(sys.argv[1])
 aos = mklist(sys.argv[2])
-x_range = range(1, min(len(soa), len(aos)) * 1000, 1000)
+#x_range = range(1, min(len(soa), len(aos)), 24)
+x_range = range(1,25)
 
+plt.xticks(range(1,25))
 plt.plot(x_range, soa, 'g--', x_range, aos, 'r--')
 plt.ylabel("duration in seconds")
-plt.xlabel("size of dataset")
+plt.xlabel("number of threads")
 plt.show()
