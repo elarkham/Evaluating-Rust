4e48224b5f389721c1e0c3bc1a07d578f8b69302
Todd Gaunt <toddgaunt@protonmail.ch>
2018-01-20 06:39:21 -0500

    Remove redundant rules from C++ make modules

--- cpp/jacobi/module.mk
+++ cpp/jacobi/module.mk
@@ -1,9 +1,3 @@
 SRC += jacobi.cpp
 TARGET += $(BUILD)/jacobi/jacobi_Os $(BUILD)/jacobi/jacobi_O2
 LDFLAGS += -lpthread
-
-$(BUILD)/jacobi/jacobi_Os: $(BUILD)/jacobi/jacobi_Os.o
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
-
-$(BUILD)/jacobi/jacobi_O2: $(BUILD)/jacobi/jacobi_O2.o
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
--- cpp/pi/module.mk
+++ cpp/pi/module.mk
@@ -1,9 +1,3 @@
 SRC += pi.cpp
 TARGET += $(BUILD)/pi/pi_Os $(BUILD)/pi/pi_O2
 LDFLAGS += -lpthread
-
-$(BUILD)/pi/pi_Os: $(BUILD)/pi/pi_Os.o
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
-
-$(BUILD)/pi/pi_O2: $(BUILD)/pi/pi_O2.o
-	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $<
