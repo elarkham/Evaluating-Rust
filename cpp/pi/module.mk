9f23f445d701d1b36ace9ec46b7bf425fbc81444
Todd Gaunt <toddgaunt@protonmail.ch>
2018-01-23 13:33:44 -0500

    Update and standardize module rules

--- cpp/pi/module.mk
+++ cpp/pi/module.mk
@@ -1,3 +1,9 @@
 SRC += pi.cpp
 TARGET += $(BUILD)/pi/pi_Os $(BUILD)/pi/pi_O2
 LDFLAGS += -lpthread
+
+$(BUILD)/pi/pi_Os: $(BUILD)/pi/pi_Os.o
+	@$(CC) $(LDFLAGS) -o $@ $<
+
+$(BUILD)/pi/pi_O2: $(BUILD)/pi/pi_O2.o
+	@$(CC) $(LDFLAGS) -o $@ $<
---
+++ cpp/wordfind/module.mk
@@ -0,0 +1,9 @@
+SRC += hash.cpp wordfind.cpp
+TARGET += $(BUILD)/wordfind/wordfind_Os $(BUILD)/wordfind/wordfind_O2
+LDFLAGS += -lpthread
+
+$(BUILD)/wordfind/wordfind_Os: $(BUILD)/wordfind/hash_Os.o $(BUILD)/wordfind/wordfind_Os.o
+	@$(CC) $(LDFLAGS) -o $@ $<
+
+$(BUILD)/wordfind/wordfind_O2: $(BUILD)/wordfind/hash_O2.o $(BUILD)/wordfind/wordfind_O2.o
+	@$(CC) $(LDFLAGS) -o $@ $<
