33ace1c0d734a20056f04b1bf5bad4900549d7be
Todd Gaunt <toddgaunt@protonmail.ch>
2018-01-23 13:35:59 -0500

    Remove many compilation errors from wordfind

--- cpp/wordfind/wordfind.cpp
+++ cpp/wordfind/wordfind.cpp
@@ -4,59 +4,50 @@
  * least 6 characters long and no more than 50 letters long.
  */
 
+#include <ctype.h>
+#include <pthread.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <string.h>
-#include <ctype.h>
 
-#include "symtab.h"
+#include "hash.h"
 
 #define PRGM_NAME "prog4"
-
-#define DPRINT(...) fprintf(stderr, PRGM_NAME": error: "__VA_ARGS__)
-
-#define EPRINT(...)\
-	{fprintf(stderr, PRGM_NAME": fatal: "__VA_ARGS__);\
-	exit(EXIT_FAILURE);}
 
 #define MAX_ARG 32
 #define MAX_WORD 50
 #define MIN_WORD 6
 
-char *getword(char *buf, int *buflen, size_t maxsize, FILE *fp);
-const char *find_largest(void *wordtab, int n);
-void *findwords(void *vargs);
-void *updateword(void *mask, void *id);
+static void
+eprint(char *fmt, ...)
+{
+	va_list ap;
 
-// Aguments for findwords()
+	fprintf(stderr, "HashTable : error: ");
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+
+	exit(EXIT_FAILURE);
+}
+
+char *getword(char *buf, int *buflen, size_t maxsize, FILE *fp);
+const char *find_largest(HashTable *wordtab, size_t n);
+void *findwords(void *vargs);
+
+/* Aguments for findwords() */
 struct findwords_args {
 	uintptr_t id;
-	void *wordtab;
+	HashTable wordtab;
 	const char *filepath;
 };
 
-// Function to pass to symtab to update the information in the hash table.
-// The file bitmask is stored directly inside the void pointer, as there is a
-// maximum limit to how many files this program can read.
-void *
-updateword(void *mask, void *id)
-{
-	uintptr_t tmp;
-	if (NULL == mask) {
-		tmp = 0;
-	} else {
-		tmp = (uintptr_t) mask;
-	}
-
-	tmp |= (1 << (uintptr_t) id);
-	return (void *)tmp;
-}
-
-// Subroutine to scan a file pointer for a completely letter-composed word.
+/* Scan a file pointer for a completely letter-composed word */
 char *
-getword(char *buf, int *buflen, size_t maxsize, FILE *fp)
+getword(char *buf, size_t *buflen, size_t maxsize, FILE *fp)
 {
 	int c = 0;
 	*buflen = 0;
@@ -88,20 +79,20 @@ findwords(void *vargs)
 	struct findwords_args *args = (struct findwords_args *)vargs;
 
 	if (!(fp = fopen(args->filepath, "r"))) {
-		DPRINT("Unable to open file (%s), killing thread.\n",
+		eprint("Unable to open file (%s), killing thread.\n",
 				args->filepath);
 		return vargs;
 	}
 
 	while (getword(buf, &buflen, MAX_WORD + 1, fp)) {
-		// Words less than MIN_WORD letters long don't matter!
 		if (MIN_WORD > buflen) {
+			/* Words shorter than MIN_WORD long don't matter! */
 			continue;
 		} else {
-			symtabUpdate(args->wordtab,
-					buf,
-					updateword,
-					(void *) args->id);
+			/* The files the word appears in is kept track of
+			 * through a bitmask, since the number of files this
+			 * program processes is limited */
+			*(args->wordtab.find(buf)) |= (1 << args->id);
 		}
 	}
 
@@ -113,12 +104,12 @@ findwords(void *vargs)
 // Iterates through the given hash table and tries to find a word that is both
 // the longest and hask the same bitmask as the integer 'n'.
 const char *
-find_largest(void *wordtab, int n)
+find_largest(HashTable *wordtab, int n)
 {
 	void *data;
 	void *iter;
 	const char *entry;
-	const char *longest;
+	const char *longest = "";
 	uintptr_t maxmask, mask;
 
 	// Find what the perfect bitmask is.
@@ -126,7 +117,7 @@ find_largest(void *wordtab, int n)
 	for (int i=0; i<n; ++i) {
 		maxmask |= 1 << i;
 	}
-
+#if 0
 	// Find the largest word with the hash-table iterator.
 	iter = symtabCreateIterator(wordtab);
 	entry = symtabNext(iter, &data);
@@ -152,6 +143,7 @@ find_largest(void *wordtab, int n)
 		entry = symtabNext(iter, &data);
 	}
 	symtabDeleteIterator(iter);
+#endif
 
 	return longest;
 }
@@ -162,32 +154,33 @@ main(int argc, char **argv)
 	size_t i;
 	pthread_t pt[MAX_ARG];
 	struct findwords_args ap[MAX_ARG];
+	size_t filecnt = argc - 1;
 
-	if (2 > argc) {
-		EPRINT("Need at least 1 file to analyze.\n");
-	} else if (MAX_ARG + 1 < argc) {
-		EPRINT("Maximum file limit (" MAX_ARG ") reached.\n");
+	if (1 > filecnt) {
+		eprint("Need at least 1 file to analyze.\n");
+	} else if (MAX_ARG < filecnt) {
+		eprint("Maximum file limit (%zu) reached.\n", MAX_ARG);
 	}
 
-	HashTable = HashTable(10000, 8, 0);
+	HashTable wordtab = HashTable(1000);
 
-	// Dispatch 1 thread per file.
-	for (size_t i = 0; i < argc - 1; ++i) {
-		// Initialize the arguments.
+	/* Dispatch 1 thread per file */
+	for (size_t i = 0; i < filecnt; ++i) {
+		/* Initialize the arguments */
 		ap[i].id = i;
 		ap[i].wordtab = wordtab;
 		ap[i].filepath = argv[i+1];
 
 		if (pthread_create(pt + i, NULL, findwords, &ap[i]))
-			EPRINT("Unable to create thread (%d).\n", i);
+			eprint("Unable to create thread (%d).\n", i);
 	}
-	// End all child threads.
-	for (size_t i = 0; i < argc - 1; ++i) {
+	/* End all child threads */
+	for (size_t i = 0; i < filecnt; ++i) {
 		if (pthread_join(pt[i], NULL))
-			EPRINT("Unable to join thread (%d).\n", i);
+			eprint("Unable to join thread (%d).\n", i);
 	}
 
-	const char *tmp = find_largest(wordtab, filecnt);
+	const char *tmp = find_largest(&wordtab, filecnt);
 
 	if (tmp) {
 		printf("%s\n", tmp);
