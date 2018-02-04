5119674f351490611a871c769595a3702a8fba61
Ethan Larkham <ethanlarkham@gmail.com>
2018-02-04 13:49:40 -0500

    Added legend to mkgraph

--- bin/mkgraph.py
+++ bin/mkgraph.py
@@ -11,16 +11,17 @@ def mklist(filename):
     return acc
 
 if (len(sys.argv) != 3):
-    print("usage: mkgraph soa aos")
+    print("usage: mkgraph cpp rust")
     exit(-1)
 
-soa = mklist(sys.argv[1])
-aos = mklist(sys.argv[2])
+cpp = mklist(sys.argv[1])
+rust = mklist(sys.argv[2])
 #x_range = range(1, min(len(soa), len(aos)), 24)
 x_range = range(1,25)
 
 plt.xticks(range(1,25))
-plt.plot(x_range, soa, 'g--', x_range, aos, 'r--')
+plt.plot(x_range, cpp, 'g--', x_range, rust, 'r--')
+plt.legend(['C++', 'Rust'])
 plt.ylabel("duration in seconds")
 plt.xlabel("number of threads")
 plt.show()
