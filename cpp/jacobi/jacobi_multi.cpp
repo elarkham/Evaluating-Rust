2e2015997a3d2a769a7fba8f0144bd4b77bfc7a3
Todd Gaunt <toddgaunt@protonmail.ch>
2018-02-19 21:54:10 -0500

    Fix indentation

--- cpp/jacobi/jacobi_multi.cpp
+++ cpp/jacobi/jacobi_multi.cpp
@@ -30,7 +30,7 @@ std::string const PROG_NAME = "jacobi";
 		exit(EXIT_FAILURE); \
 	} while (0)
 
-#define SIZE    2560
+#define SIZE    5120
 #define TEMP    50.0
 #define EPSILON 0.1
 
@@ -43,24 +43,24 @@ double old_p[SIZE][SIZE];
 void* work(void* in)
 {
 	size_t id  = (size_t)in;
-  size_t beg = id * split;
-  size_t end = beg + split;
+	size_t beg = id * split;
+	size_t end = beg + split;
 
-  if (id == 0) {
-    beg += 1;
-  }
-  if (id == (n_threads - 1)) {
-    end -= 1;
-  }
+	if (id == 0) {
+		beg += 1;
+	}
+	if (id == (n_threads - 1)) {
+		end -= 1;
+	}
 
-  for (size_t i = beg; i < end; i++) {
-    for (size_t j = 1; j < SIZE; j++) {
-      new_p[i][j] = ( old_p[i-1][j]
-                    + old_p[i+1][j]
-                    + old_p[i][j+1]
-                    + old_p[i][j-1] ) / 4.0;
-    }
-  }
+	for (size_t i = beg; i < end; i++) {
+		for (size_t j = 1; j < SIZE; j++) {
+			new_p[i][j] = ( old_p[i-1][j]
+					+ old_p[i+1][j]
+					+ old_p[i][j+1]
+					+ old_p[i][j-1] ) / 4.0;
+		}
+	}
 
 	return 0;
 }
@@ -76,83 +76,83 @@ int main(int argc, char* argv[])
 {
 	struct timespec start, stop;
 
-  double maxerr;
+	double maxerr;
 	double change;
 	size_t i,j;
 	int cool_cells;
 
-  /* Get thread number from first argument */
-  if (argc !=2) {
-    usage();
-  }
-  n_threads = atoi(argv[1]);
-  if (n_threads <= 0)
-    EPRINT("Less than 0 child threads requested");
-  split = SIZE / n_threads;
+	/* Get thread number from first argument */
+	if (argc !=2) {
+		usage();
+	}
+	n_threads = atoi(argv[1]);
+	if (n_threads <= 0)
+		EPRINT("Less than 0 child threads requested");
+	split = SIZE / n_threads;
 
-  /* Array for storing thread IDs */
+	/* Array for storing thread IDs */
 	clock_gettime(CLOCK_MONOTONIC, &start);
-  std::vector<pthread_t> ptid;
-  ptid.reserve(n_threads);
+	std::vector<pthread_t> ptid;
+	ptid.reserve(n_threads);
 
-  for (i = 0; i < n_threads; i++) {
-    if (pthread_create(&ptid[i], NULL, work, (void *) i) != 0)
-      EPRINT("Could not create thread");
-  }
+	for (i = 0; i < n_threads; i++) {
+		if (pthread_create(&ptid[i], NULL, work, (void *) i) != 0)
+			EPRINT("Could not create thread");
+	}
 
-  /* north, east, and west boundaries */
+	/* north, east, and west boundaries */
 	for (i = 0; i < SIZE; i++)
-  {
+	{
 		old_p[0][i] = new_p[0][i] = 0.0;
 		old_p[i][0] = new_p[i][0] = 0.0;
 		old_p[i][SIZE-1] = new_p[i][SIZE-1] = 0.0;
 	}
 
-  /* south boundary */
+	/* south boundary */
 	for (i = 1; i < SIZE-1; i++)
 	{
-    old_p[SIZE-1][i] = new_p[SIZE-1][i] = 100.0;
+		old_p[SIZE-1][i] = new_p[SIZE-1][i] = 100.0;
 	}
 
-  /* inner core */
+	/* inner core */
 	for (i = 1; i < SIZE-1; i++) {
-    for (j = 1; j < SIZE-1; j++) {
+		for (j = 1; j < SIZE-1; j++) {
 			new_p[i][j] = 50.0;
 			old_p[i][j] = 50.0;
 		}
 	}
 
-  /* Compute steady-state temperatures */
+	/* Compute steady-state temperatures */
 	do {
-    /* delegate work to threads */
-    for(i = 0; i < n_threads; i++) {
-	    if(pthread_create(&ptid[i], NULL, work, (void *) i) != 0)
-        EPRINT("Could not create thread");
-    }
-    for (size_t i = 0; i < n_threads; ++i) {
-      if (pthread_join(ptid[i], NULL))
-        EPRINT("Could not join thread");
-    }
+		/* delegate work to threads */
+		for(i = 0; i < n_threads; i++) {
+			if(pthread_create(&ptid[i], NULL, work, (void *) i) != 0)
+				EPRINT("Could not create thread");
+		}
+		for (size_t i = 0; i < n_threads; ++i) {
+			if (pthread_join(ptid[i], NULL))
+				EPRINT("Could not join thread");
+		}
 
-    /* update maxerr */
+		/* update maxerr */
 		maxerr = 0.0;
 		for (i = 1; i < SIZE-1; i++) {
 			for (j = 1; j < SIZE-1; j++)
-      {
+			{
 				change = fabs(old_p[i][j] - new_p[i][j]);
 				if (maxerr < change){
-          maxerr = change;
-        }
+					maxerr = change;
+				}
 			}
-    }
+		}
 
-    /* transfer new to old */
+		/* transfer new to old */
 		for (i = 1; i < SIZE-1; i++) {
 			for (j = 1; j < SIZE-1; j++)
-      {
+			{
 				old_p[i][j] = new_p[i][j];
 			}
-    }
+		}
 	} while (maxerr > EPSILON);
 
 	cool_cells = 0;
@@ -160,13 +160,13 @@ int main(int argc, char* argv[])
 		for (j = 0; j < SIZE; j++)
 			if (new_p[i][j] < TEMP) cool_cells++;
 
-  /* end timer */
+	/* end timer */
 	clock_gettime(CLOCK_MONOTONIC, &stop);
 	std::cout << (double) (stop.tv_nsec - start.tv_nsec) / 1000000000 +
-			(double) (stop.tv_sec - start.tv_sec) << std::endl;
+		(double) (stop.tv_sec - start.tv_sec) << std::endl;
 
 	fprintf(stderr,
-      "There are %d cells cooler than %5.2f degrees\n", cool_cells, TEMP);
+			"There are %d cells cooler than %5.2f degrees\n", cool_cells, TEMP);
 
 	return 0;
 }
