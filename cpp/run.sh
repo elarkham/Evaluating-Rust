95cccba971edcf92d1cb623a5e62e107215645d6
Ethan Larkham <ethanlarkham@gmail.com>
2018-04-09 10:02:51 -0400

    Updated run scripts to handle wordfind

--- cpp/run.sh
+++ cpp/run.sh
@@ -10,10 +10,20 @@ if [ "" == "$1" ]; then
 	exit -1
 fi
 mkdir -p ${OUTPUT}
-rm -rf ${OUTPUT}/cpp_${NAME}_*.txt
+rm -rf ${OUTPUT}/cpp_$NAME_*.txt
 
 INTERVALS=$(eval echo {1..$N_INTERVALS})
 THREADS=$(eval echo {1..$N_THREADS})
+
+if [ "$NAME" == "wordfind" ]; then
+  for i in $INTERVALS; do
+  	for j in $THREADS; do
+      ./build/$NAME/${NAME}_O2 $(eval echo ../data/wordfind/alice_{1..$j}.txt) >> ${OUTPUT}/cpp_${NAME}_$i.txt
+  	done
+  done
+  exit;
+fi
+
 for i in $INTERVALS; do
 	for j in $THREADS; do
 		./build/$NAME/${NAME}_O2 $j >> ${OUTPUT}/cpp_${NAME}_$i.txt
--- rust/run.sh
+++ rust/run.sh
@@ -10,10 +10,20 @@ if [ "" == "$1" ]; then
 	exit -1
 fi
 mkdir -p ${OUTPUT}
-rm -rf ${OUTPUT}/rust_${NAME}*.txt
+rm -rf ${OUTPUT}/rust_$NAME_*.txt
 
 INTERVALS=$(eval echo {1..$N_INTERVALS})
 THREADS=$(eval echo {1..$N_THREADS})
+
+if [ "$NAME" == "wordfind" ] || [ "$NAME" == "wordfind_batch" ]; then
+  for i in $INTERVALS; do
+  	for j in $THREADS; do
+      ./$NAME/target/release/$NAME $(eval echo ../data/wordfind/alice_{1..$j}.txt) >> ${OUTPUT}/rust_${NAME}_$i.txt
+  	done
+  done
+  exit;
+fi
+
 for i in $INTERVALS; do
 	for j in $THREADS; do
 		./$NAME/target/release/$NAME $j >> ${OUTPUT}/rust_${NAME}_$i.txt
